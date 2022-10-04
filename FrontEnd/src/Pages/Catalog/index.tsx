8ef7b8a8181473e0322d012cb2660e9ec75c31d7
Eduardo Oliveira Ramos <eduardo.oliveiraramos12@hotmail.com>
2022-10-03 21:54:27 -0300

    product price

--- FrontEnd/src/Pages/Catalog/index.tsx
+++ FrontEnd/src/Pages/Catalog/index.tsx
@@ -1,10 +1,28 @@
 import Navbar from 'components/Navbar/intex';
+import ProductCard from 'components/ProductCard';
 
 const Catalog = () => {
   return (
     <>
       <Navbar />
       <div className="container my-4">
+        <div className="row">
+          <div className="col-sm-6 col-lg-3">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-3">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-3">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-3">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6 col-lg-3">
+            <ProductCard />
+          </div>
+        </div>
       </div>
     </>
   );
